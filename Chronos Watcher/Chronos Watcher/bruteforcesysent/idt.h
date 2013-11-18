990895f6bb90d035c2a8744281122f4507a801e6
Vlad Ionescu <vlad@Vlads-MacBook-Pro.local>
2013-11-17 21:51:18 -0500

    Added bruteforcesysent with modifications, began coding

---
+++ Chronos Watcher/Chronos Watcher/bruteforcesysent/idt.h
@@ -0,0 +1,34 @@
+/*
+ *     _____                                    
+ *  __|___  |__  _____   __   _    __    ______ 
+ * |      >    ||     | |  | | | _|  |_ |   ___|
+ * |     <     ||     \ |  |_| ||_    _||   ___|
+ * |______>  __||__|\__\|______|  |__|  |______|
+ *    |_____|                                   
+ *     _____                                    
+ *  __|___  |__  _____  _____   ______  ______  
+ * |   ___|    |/     \|     | |   ___||   ___| 
+ * |   ___|    ||     ||     \ |   |__ |   ___| 
+ * |___|     __|\_____/|__|\__\|______||______| 
+ *    |_____|    
+ *
+ * Bruteforce Sysent
+ *
+ * (c) 2012, fG! - reverser@put.as - http://reverse.put.as
+ *
+ * idt.h
+ *
+ */
+
+#ifndef _idt_h_
+#define _idt_h_
+
+#include <stdint.h>
+
+typedef uint64_t idt_t;
+
+// prototypes
+int8_t get_kernel_type (void);
+idt_t  get_addr_idt (uint8_t kernel_type);
+
+#endif
