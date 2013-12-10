fb82a48fe062d1fb7f66ea22e99acaba17306a84
Vlad Ionescu <vlad@vladionescu.com>
2013-12-09 20:32:02 -0800

    The gods hungered, and finally, the kext compiled.

--- Chronos Watcher/Chronos Watcher/Chronos_Watcher.c
+++ Chronos Watcher/Chronos Watcher/Chronos_Watcher.c
@@ -12,10 +12,43 @@
 #include <mach/mach_types.h>
 #include <string.h>
 
-// thanks to: gdbinit a/k/a fractalG!
-// this is for dev right now - will make leaner soon
-#include "bruteforcesysent/sysent.h"
-#include "bruteforcesysent/idt.h"
+struct sysent {		/* system call table */
+	int16_t		sy_narg;	/* number of args */
+	int8_t		sy_resv;	/* reserved  */
+	int8_t		sy_flags;	/* flags */
+	uint32_t	sy_call;	/* implementing function */
+	uint32_t	sy_arg_munge32; /* system call arguments munger for 32-bit process */
+	uint32_t	sy_arg_munge64; /* system call arguments munger for 64-bit process */
+	int32_t		sy_return_type; /* system call return types */
+	uint16_t	sy_arg_bytes;	/* Total size of arguments in bytes for
+								 * 32-bit system calls
+								 */
+};
+struct sysent64 {		/* system call table */
+	int16_t		sy_narg;	/* number of args */
+	int8_t		sy_resv;	/* reserved  */
+	int8_t		sy_flags;	/* flags */
+    uint32_t    padding;        /* padding, x86 binary against 64bits kernel would fail */
+	uint64_t	sy_call;	/* implementing function */
+	uint64_t	sy_arg_munge32; /* system call arguments munger for 32-bit process */
+	uint64_t	sy_arg_munge64; /* system call arguments munger for 64-bit process */
+	int32_t		sy_return_type; /* system call return types */
+	uint16_t	sy_arg_bytes;	/* Total size of arguments in bytes for
+								 * 32-bit system calls
+								 */
+};
+
+// 16 bytes IDT descriptor, used for 32 and 64 bits kernels (64 bit capable cpus!)
+struct descriptor_idt
+{
+	uint16_t offset_low;
+	uint16_t seg_selector;
+	uint8_t reserved;
+	uint8_t flag;
+	uint16_t offset_middle;
+	uint32_t offset_high;
+	uint32_t reserved2;
+};
 
 kern_return_t Chronos_Watcher_start(kmod_info_t * ki, void *d);
 kern_return_t Chronos_Watcher_stop(kmod_info_t *ki, void *d);
@@ -43,12 +76,13 @@ kern_return_t Chronos_Watcher_start(kmod_info_t * ki, void *d)
     strcpy(syscalls[10], "pipe");
     strcpy(syscalls[11], "truncate");
     
-    // search for sysent pointer in kernel memory (/dev/kmem)
-    uint64_t sysent = get_sysent_address();
+    // sysent pointer in kernel memory (/dev/kmem)
+    // get by running ./bruteforcesysent
+    struct sysent64 *sysent = (struct sysent64 *)0xffffff802f85d7f0;
     
     // search for pointers to every SYSCALL we want to modify (foreach SYSCALL)
     for (int i=0;i<sizeof(*syscalls);i++) {
-        uint64_t syscall_ptr = sysent[*syscalls[i]].sy_call;
+        uint64_t syscall_ptr = sysent[(int)*syscalls[i]].sy_call;
         uint64_t syscall_ptr_o = syscall_ptr;
         
         // save the original sysent[SYSCALL].sy_call contents in one of the global variables above
